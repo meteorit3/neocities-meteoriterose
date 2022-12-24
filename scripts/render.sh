7ab20d750941d924b1caced35e0eecfb06511b20
kamacit3 <pearlit3@gmail.com>
2022-12-23 16:47:08 -0800

    fixed the rendering thing so it does it good

--- scripts/render.sh
+++ scripts/render.sh
@@ -3,5 +3,5 @@ ROOTDIR="$HOME/Git/neocities-meteoriterose"
 php -S localhost:8008 -t $ROOTDIR &
 php_pid=$!;
 sleep 1;
-wget -rk localhost:8008 -nH -P $ROOTDIR/render_result;
+wget -rkE localhost:8008 -nH -P $ROOTDIR/render_result;
 kill $php_pid;
