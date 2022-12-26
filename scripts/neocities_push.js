db61dc0029ad6c6f69b96cbc77ac037e7578c860
kamacit3 <pearlit3@gmail.com>
2022-12-26 01:07:54 -0800

    made the http request next gotta figure out how to parse it

--- scripts/neocities_push.js
+++ scripts/neocities_push.js
@@ -1,1 +1,15 @@
 // get all the hashes from neocities / delete any that dont match the ones of the local version / push the rest
+
+const doenv = require('dotenv').config() // include .env file (NC_USER/NC_PASS for API)
+const axios = require('axios');
+
+//var apiurl = 'https://' + process.env.API_KEY + "@neocities.org/api/list"
+axios.get('https://neocities.org/api/list', {
+    headers: {"Authorization":"Bearer "+process.env.API_KEY}
+}).then(function (response) {
+    console.log("Response"+response)
+}).catch(function (error) {
+    console.log("Error"+error) 
+}).finally(function () {
+
+})
