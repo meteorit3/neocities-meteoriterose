e13ef1999b197e14448c3f694207fb4041fe34a6
kamacit3 <pearlit3@gmail.com>
2022-12-30 22:51:41 -0800

    fuck you kills yours self

--- scripts/neocities_push.js
+++ scripts/neocities_push.js
@@ -3,9 +3,8 @@ const axios = require('axios') //i pro8ly didnt need this 8ut too L8 ::::3
 const Formdata = require('form-data')
 
 const fs = require('fs')
-const { readdir } = require('fs').promises;
-const crypto = require('crypto');
-
+const { readdir } = require('fs').promises
+const crypto = require('crypto')
 
 const pRecursiveList = async (dir) => {
     let files = []
@@ -49,8 +48,9 @@ const pLocalList = async (dir) => {
 }
 const pNeocitiesList = async () => {
     return axios.get('https://neocities.org/api/list', {
-        headers: { "Authorization": "Bearer " + process.env.AUTH, },
-        responseType: "json",
+        headers: {
+            Authorization: "Bearer " + process.env.API_KEY,
+        }
     }).then(response => {
         transformed_data = {}
         for (const f in response.data["files"]) {
@@ -68,30 +68,52 @@ const pNeocitiesList = async () => {
             8ar: {"is_directory": true}
         }
          so its easier 2 deal with l8er watever u Getit */
-        return(transformed_data)
+        return (transformed_data)
     }).catch(error => {
-        console.log(error)
+        //console.log(error)
     })
 }
 
-const pNeocitiesUpload = async (filepaths /* array */) => {
+const pUpload = async (filepaths) => {
     //send a POST request to /api/upload using a form with the file name and a stream of the file data attached to it
     //like this: form.append(files[i].name, fs.createReadStream(files[i].path))
     const form = new Formdata()
     for (f in filepaths) {
         let name = filepaths[f].split("/").pop() //this does mean u cant use '/' in the filename 8ut whatever Who Care
         let stream = fs.createReadStream(filepaths[f])
-        form.append(name, stream)
+        form.append(name, stream, filepaths[f])
     }
     //now just send a form with form.getHeaders()
-    return axios.post('https://neocities.org/api/list', {
+    return axios({
+        method: 'post',
+        url: 'https://neocities.org/api/upload',
         headers: {
             ...form.getHeaders(),
-            "Authorization": "Bearer " + process.env.AUTH,
         },
+        auth: process.env.NC_USER + ':' + process.env.NC_PASS,
     }).then(response => {
-        console.log(response)
-        return(response)
+        console.log(response.data)
+        return (response)
+    }).catch(error => {
+        console.log(error)
+    })
+}
+
+const pDelete = async (filepaths) => {
+    const form = new Formdata()
+    for (f in filepaths) {
+        form.append('filenames[]', filepaths[f])
+    }
+    return axios({
+        method: 'post',
+        url: 'https://neocities.org/api/delete',
+        headers: {
+            ...form.getHeaders(),
+        },
+        auth: process.env.NC_USER + ':' + process.env.NC_PASS,
+    }).then(response => {
+        console.log(response.data)
+        return (response)
     }).catch(error => {
         console.log(error)
     })
@@ -107,7 +129,6 @@ Promise.all([pLocalList('.'), pNeocitiesList()]).then(twoofthem => {
     // we get an O8ject containing 8oth our locally stored file list and the remote file list [0 and 1]
     // filter out any files that match (files that havent changed since last push, so the hash is the same),
     // and upload / delete all the remaining ones, respectively
-    console.log(twoofthem)
     for (const f in twoofthem[0]) {
         //does this file exist, and do the hashes match?
         if (twoofthem[1][f]["sha1_hash"] == twoofthem[0][f]["sha1_hash"]) {
@@ -115,5 +136,11 @@ Promise.all([pLocalList('.'), pNeocitiesList()]).then(twoofthem => {
             delete twoofthem[1][f] //i.e. dont delete this file
         }
     }
-
+    console.log(twoofthem)
+    return (twoofthem)
+}).then(twoofthem => {
+    pDelete(Object.keys(twoofthem[1]))
+    return twoofthem
+}).then(twoofthem => {
+    pUpload(Object.keys(twoofthem[0]))
 })
